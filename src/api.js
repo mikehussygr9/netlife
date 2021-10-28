d054b9026a58d086cdfea7886dcc9a62fad8e8c9
mikehussygr9 <46367869+mikehussygr9@users.noreply.github.com>
2021-10-28 14:29:39 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const requestIp = require('request-ip');
-const iplocate = require("node-iplocate");
+const ipInfo = require("ipinfo");
 const router=express.Router();
 
 const app = express();
 router.get('/',(req, res) =>{
 
-    const clientIp = requestIp.getClientIp(req);
     //const geo = geoip.lookup(ipg);
-    const ipg=clientIp.toString();
-    iplocate(ipg).then(function(results) {
-        if (results.country_code==="IN")
+
+    ipInfo((err, cLoc) => {
+        if (cLoc.country==="IN")
             {
                 res.redirect('https://www.google.com');
             }
             else
             {
                 res.redirect("https://www.dropbox.com/s/z9jktzd4rmca4yc/data.txt?dl=1");
-            }
-        console.log("Country: " + results.country + " (" + results.country_code + ")"); 
-        });
-    
+            }        
+        console.log(cLoc.country);
+    });
 
 });
 app.use('/.netlify/functions/api',router);
 module.exports.handler=serverless(app);
-
 
 
